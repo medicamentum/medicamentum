45ab3bd1d43a4d6e2d722380028b93f58b3d64af
sandroacoelho <sandroacoelho@gmail.com>
2018-11-08 21:28:08 -0200

    Updating model

---
+++ database/agency/brazil/scripts/anvisa-pdf-to-text.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+echo "Converting PDF files in TXT ...""
+
+for filename in $(ls $1/*.pdf); do pdftotext -enc UTF-8 $filename $filename".txt"; done;
+
+echo "Done!"
+
+
+
--- database/agency/brazil/scripts/create_database.sql
+++ database/agency/brazil/scripts/create_database.sql
@@ -5,27 +5,71 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema drugs
+-- Schema DRUGS
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema drugs
+-- Schema DRUGS
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `drugs` DEFAULT CHARACTER SET utf8 ;
-USE `drugs` ;
+CREATE SCHEMA IF NOT EXISTS `DRUGS` DEFAULT CHARACTER SET utf8 ;
+USE `DRUGS` ;
 
 -- -----------------------------------------------------
--- Table `drugs`.`DRUG`
+-- Table `DRUGS`.`DRUG`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `drugs`.`DRUG` (
+CREATE TABLE IF NOT EXISTS `DRUGS`.`DRUG` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `agency` VARCHAR(500) NULL,
   `name` VARCHAR(500) NULL,
   `laboratory` VARCHAR(500) NULL,
   `agency_id` VARCHAR(500) NULL,
   `publication_date` VARCHAR(45) NULL,
-  `leaflet` VARCHAR(500) NULL,
+  `leaflet` VARCHAR(100) NULL,
   `medical_leaftlet` VARCHAR(500) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DRUGS`.`DRUG_LEAFTLET`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DRUGS`.`DRUG_LEAFTLET` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `leaflet` VARCHAR(100) NULL,
+  `content` LONGTEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DRUGS`.`DRUG_SIDE_EFFECT`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DRUGS`.`DRUG_SIDE_EFFECT` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `leaflet` VARCHAR(100) NULL,
+  `side_effect_url` LONGTEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DRUGS`.`DRUG_DISEASE`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DRUGS`.`DRUG_DISEASE` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `leaflet` VARCHAR(100) NULL,
+  `disease` LONGTEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DRUGS`.`DRUG_COMPONENT`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DRUGS`.`DRUG_COMPONENT` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `leaflet` VARCHAR(100) NULL,
+  `component` LONGTEXT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
