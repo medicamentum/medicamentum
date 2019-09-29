65bf942969baf330074d695dd5ee30cb50f141ee
sacoelho@translucent.com <sacoelho@translucent.com>
2019-09-29 15:54:52 -0300

    Normalize unicode value

--- database/agency/brazil/anvisa/insert-text-files.py
+++ database/agency/brazil/anvisa/insert-text-files.py
@@ -4,13 +4,15 @@ import sys
 import os
 import io
 import mysql.connector
+import unicodedata
 
 mydb = mysql.connector.connect(
   host=sys.argv[1],
   user=sys.argv[2],
   passwd=sys.argv[3],
   database="DRUGS",
-  use_unicode=True
+  use_unicode=True,
+  charset='utf8'
 )
 
 path = sys.argv[4] + "/*.txt"
@@ -18,7 +20,11 @@ path = sys.argv[4] + "/*.txt"
 mycursor = mydb.cursor()
 
 for file in glob.glob(path):
+
     content = io.open(file, mode="r", encoding="utf-8").read()
+    content = content.strip()
+    content = unicodedata.normalize("NFKC", content)
+
     directory, filename = os.path.split(file)
     filename = filename.replace(".txt", "")
     sql = "INSERT INTO DRUG_LEAFTLET (leaflet, content) VALUES (%s, %s)"
